950d37bb133b982d67bec7a70cf0d7c31ec0b2a7
Unum Sarfraz <anamsarfraz@gmail.com>
2017-03-19 13:43:35 -0700

    Added vector drawable for all images except share

--- app/src/main/java/com/codepath/nytsearch/adapters/ArticleAdapter.java
+++ app/src/main/java/com/codepath/nytsearch/adapters/ArticleAdapter.java
@@ -1,8 +1,6 @@
 package com.codepath.nytsearch.adapters;
 
 import android.content.Context;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
